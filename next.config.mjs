62908090f4a0d32f22f07f2e7431581976747373
Roro253 <roronazari25@gmail.com>
2025-09-07 08:41:00 -0700

    fix: configs and deps for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
       { protocol: "https", hostname: "v5.airtableusercontent.com", pathname: "/**" },
       { protocol: "https", hostname: "v2.airtableusercontent.com", pathname: "/**" },
       { protocol: "https", hostname: "dl.airtable.com", pathname: "/**" },
+      { protocol: "https", hostname: "logo.clearbit.com", pathname: "/**" },
     ],
   },
   // JBV: END images allowlist
